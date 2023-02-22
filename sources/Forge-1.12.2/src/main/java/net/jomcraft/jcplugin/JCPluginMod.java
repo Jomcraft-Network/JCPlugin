ba2985606fcb486029df65930d7936420f60da5c
PT400C Jomcraft <pt400c@gmail.com>
2023-02-22 17:44:15 +0100

    Initial testing release for Forge 1.12.2

---
+++ sources/Forge-1.12.2/src/main/java/net/jomcraft/jcplugin/JCPluginMod.java
@@ -0,0 +1,18 @@
+package net.jomcraft.jcplugin;
+
+import net.minecraftforge.fml.common.Mod;
+
+//@Mod(modid = JCPluginMod.MODID, acceptedMinecraftVersions = "[1.7.10,1.12.2]", name = JCPluginMod.NAME, version = JCPluginMod.VERSION, clientSideOnly = true)
+public class JCPluginMod {
+
+    @Mod.Instance
+    public static JCPluginMod instance;
+
+    public static final String MODID = "jcplugin";
+    public static final String NAME = "JCPlugin";
+    public static final String VERSION = "2.8.7";
+
+    public JCPluginMod() {
+        instance = this;
+    }
+}
